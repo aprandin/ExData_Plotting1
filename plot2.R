9fcf090879d5daf33da27825dcd686c8c26c69f9
aprandin <albyprandini@gmail.com>
2021-02-07 15:37:06 +0100

    Uploading plot2

---
+++ plot2.R
@@ -0,0 +1,16 @@
+# Reading the data
+mydata <- read.csv2('household_power_consumption.txt', na.strings = '?')
+mydata$Time <- strptime(paste(mydata$Date,mydata$Time),'%d/%m/%Y %H:%M:%S')
+
+mydata$Date <- as.Date(mydata$Date, '%d/%m/%Y')
+mydata_small <- mydata[(mydata$Date >= '2007-02-01' & mydata$Date <= '2007-02-02'),]
+for (i in 3:9) {
+      mydata_small[,i] <- as.numeric(mydata_small[,i])
+}
+
+# Construct the plot
+plot(x = mydata_small$Time, y = mydata_small$Global_active_power, type = 'l', xlab = '' ,ylab = 'Global Active Power (kilowatts)')
+
+# Copy the plot to a PNG file
+dev.copy(png, file = 'plot2.png')
+dev.off()
