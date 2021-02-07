5c65939b845213940b4a84c51b102b1aeb29bc55
aprandin <albyprandini@gmail.com>
2021-02-07 16:25:54 +0100

    Annotation modified

--- plot3.R
+++ plot3.R
@@ -16,5 +16,5 @@ with(mydata_small, lines(x = Time, y = Sub_metering_2, col = 'red'))
 with(mydata_small, lines(x = Time, y = Sub_metering_3, col = 'blue'))
 legend('topright', legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), lty = c(1,1), lwd = c(1,1), col = c('black','red','blue'))
 
-# Copy the plot to a PNG file
+# Closing the graphics device
 dev.off()
