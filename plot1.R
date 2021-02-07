e5af15787ed444316d3ae825fffbe648bd3ba114
aprandin <albyprandini@gmail.com>
2021-02-07 15:36:35 +0100

    Changing date format

--- plot1.R
+++ plot1.R
@@ -1,18 +1,14 @@
 # Reading the data
-library(lubridate)
-mydata <- read.csv2('household_power_consumption.txt')
-mydata$Date <- dmy(mydata$Date)
-mydata$Time <- hms(mydata$Time)
+mydata <- read.csv2('household_power_consumption.txt', na.strings = '?')
+mydata$Date <- as.Date(mydata$Date, '%d/%m/%Y')
 
 mydata_small <- mydata[(mydata$Date >= as.Date('2007-02-01', '%Y-%m-%d') & mydata$Date <= as.Date('2007-02-02', '%Y-%m-%d')),]
 for (i in 3:9) {
       mydata_small[,i] <- as.numeric(mydata_small[,i])
 }
 
-mydata_complete <- mydata_small[complete.cases(mydata_small$Global_active_power),]
-
 # Construct the plot
-hist(mydata_complete$Global_active_power, main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', col = 'red')
+hist(mydata_small$Global_active_power, main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', col = 'red')
 
 # Copy the plot to a PNG file
 dev.copy(png, file = 'plot1.png')
